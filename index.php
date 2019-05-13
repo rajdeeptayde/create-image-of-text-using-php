90317e8780bf6fdba929c09a44320c8ef6a35f3b
Rajdeep Tayde <rajdeeptayde@gmail.com>
2019-05-13 12:34:01 +0530

    index.php Added

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+/*$img = imagecreatetruecolor(200, 200);
+imagesavealpha($img, true);
+$color = imagecolorallocatealpha($img, 0, 0, 0, 127);
+imagefill($img, 0, 0, $color);
+imagepng($img, 'test.png');
+*/
+
+ 	$font = 25;
+    $string = 'Rajdeep Tayde';
+    $im = @imagecreatetruecolor(strlen($string) * $font / 1.5, $font);
+    imagesavealpha($im, true);
+    imagealphablending($im, false);
+    $white = imagecolorallocatealpha($im, 255, 255, 255, 127);
+    imagefill($im, 0, 0, $white);
+    $lime = imagecolorallocate($im, 204, 255, 51);
+    imagestring($im, $font, 0, 0,$string,$lime );
+    /*imagepng($im, 'test.png');*/
+    header("Content-type: image/png");
+    imagepng($im);
+    imagedestroy($im);
